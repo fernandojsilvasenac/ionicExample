53ca775e7251f1107fe8beb3bb1b329eba37d020
FERNANDO JACINTO DA SILVA <fernando.jsilva9@senacsp.edu.br>
2020-03-06 20:34:52 -0300

    my firt commit

---
+++ src/app/model/artigo.ts
@@ -0,0 +1,11 @@
+export class Artigo{
+    
+    constructor(
+        public id:number,
+        public titulo:string,
+        public autor:string,
+        public data:string,
+        public imgUrl:string,
+        public texto:string){}
+
+}
